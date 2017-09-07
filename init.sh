634b1fc4e0fe54454b45bbef1155892e46765ade
Charles Gillham <charlesgillham@icloud.com>
2017-09-07 10:17:41 +0100

    Moved to init.sh & began working with cloudformation (WIP)

--- init.sh
+++ init.sh
@@ -38,26 +38,41 @@ function edit_wp_config {
 
 function create_wp_zip {
   cd wordpress
-  zip -r ../wordpress.zip .
+  zip -r ../wordpress.zip . > /dev/null 2>&1
+  cd ..
   rm -rf wordpress/
 }
 
-if [ "$1" == ""]; then
+
+function create_cloud_formation {
+
+  if ! aws s3 ls s3://$2/ > /dev/null 2>&1 ; then
+    aws s3 mb s3://$2 --region $3
+  fi
+  aws s3 rm s3://$2/wordpress.zip
+  aws s3 cp wordpress.zip s3://$2/
+
+  aws cloudformation create-stack --template-body file://formation.yaml --stack-name wordpress-$(date "+%Y-%m-%d-%H-%M-%S") --parameters ParameterKey=wordpressDBName,ParameterValue=wordpress ParameterKey=wordpressDBUser,ParameterValue=wordpress ParameterKey=wordpressDBPass,ParameterValue=$1 ParameterKey=S3CodeStorageBucketName,ParameterValue=$2 ParameterKey=S3BucketName,ParameterValue=wordpress-$(date "+%Y-%m-%d-%H-%M-%S")
+}
+
+
+if [ "$1" == "" ]; then
   secret=$(openssl rand -hex 64)
 else
   secret=$1
 fi
 
+dbPass=$(openssl rand -hex 16)
+S3StorageBucketName="wordpress-aws-code-storage"
+awsRegion="eu-west-2"
+
 download_wordpress
-
 move_htaccess
-
 edit_htaccess $secret
-
 install_wordpress_plugins
-
 edit_wp_config
-
 create_wp_zip
+create_cloud_formation $dbPass $S3StorageBucketName $awsRegion
 
+echo "Database Password:" $dbPass
 echo "Access Key:" $secret
